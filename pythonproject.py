872c0826e652ed8fd9e0392aea4e4e1c96903dff
Erik Sabin <eriksabin97@gmail.com>
2023-02-11 08:11:46 -0500

    Made functions

---
+++ pythonproject.py
@@ -0,0 +1,10 @@
+def hello():
+    print("Hello")
+    
+def pac(one,two, three):
+    mylist = [one,two,three]
+    return mylist
+
+def list():
+    mylist =["food", "sleep"]
+    print(f"{mylist[1]} is so good and I like {mylist[2]}")
